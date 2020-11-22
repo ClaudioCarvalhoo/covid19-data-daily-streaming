25ac9b9ccf3b2a5c7700a99e400b918a3275f556
Claudio Carvalho <carvalhooclaudio@gmail.com>
2020-11-22 20:39:20 -0300

    feat: map

--- covid19-dashboard/src/views/UserProfile/UserProfile.js
+++ covid19-dashboard/src/views/UserProfile/UserProfile.js
@@ -1,17 +1,16 @@
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import avatar from "assets/img/faces/marc.jpg";
 import Card from "components/Card/Card.js";
-import CardAvatar from "components/Card/CardAvatar.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 import CardHeader from "components/Card/CardHeader.js";
-import Button from "components/CustomButtons/Button.js";
 import GridContainer from "components/Grid/GridContainer.js";
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import React from "react";
+import { useSelector } from "react-redux";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import interpolate from "color-interpolate";
 
 const styles = {
   cardCategoryWhite: {
@@ -38,8 +37,17 @@ export default function UserProfile() {
   const classes = useStyles();
   const geoUrl =
     "https://gist.githubusercontent.com/ruliana/1ccaaab05ea113b0dff3b22be3b4d637/raw/196c0332d38cb935cfca227d28f7cecfa70b412e/br-states.json";
+  const data = useSelector((state) => state.dashboard);
+  let colormap = interpolate([
+    "#393A3C",
+    "#5D5F5C",
+    "#F07249",
+    "#D22C2C",
+    "#6E1B09",
+  ]);
   function makeFillForState(state_acro) {
-    return "";
+    let n = 10;
+    return colormap(n / 1200000);
   }
 
   return (
