9af2efa95fc8698bc8d1a6a90f19314393181ecd
mvgmb <mv3gmb@gmail.com>
2020-11-23 17:42:38 -0300

    feat(cobiv19-dashboard): break monthly chart into two charts

--- covid19-dashboard/src/views/Dashboard/Dashboard.js
+++ covid19-dashboard/src/views/Dashboard/Dashboard.js
@@ -1,28 +1,28 @@
-import { Icon, Typography } from "@material-ui/core";
+import { Icon, Typography } from '@material-ui/core';
 // @material-ui/core
-import { makeStyles } from "@material-ui/core/styles";
-import AccessTime from "@material-ui/icons/AccessTime";
-import ArrowUpward from "@material-ui/icons/ArrowUpward";
-import { default as Warning } from "@material-ui/icons/Warning";
-import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
-import Chartist from "chartist";
-import Card from "components/Card/Card.js";
-import CardBody from "components/Card/CardBody.js";
-import CardFooter from "components/Card/CardFooter.js";
-import CardHeader from "components/Card/CardHeader.js";
-import CardIcon from "components/Card/CardIcon.js";
-import GridContainer from "components/Grid/GridContainer.js";
+import { makeStyles } from '@material-ui/core/styles';
+import AccessTime from '@material-ui/icons/AccessTime';
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import { default as Warning } from '@material-ui/icons/Warning';
+import styles from 'assets/jss/material-dashboard-react/views/dashboardStyle.js';
+import Chartist from 'chartist';
+import Card from 'components/Card/Card.js';
+import CardBody from 'components/Card/CardBody.js';
+import CardFooter from 'components/Card/CardFooter.js';
+import CardHeader from 'components/Card/CardHeader.js';
+import CardIcon from 'components/Card/CardIcon.js';
+import GridContainer from 'components/Grid/GridContainer.js';
 // core components
-import GridItem from "components/Grid/GridItem.js";
-import Table from "components/Table/Table.js";
-import Danger from "components/Typography/Danger.js";
-import React from "react";
+import GridItem from 'components/Grid/GridItem.js';
+import Table from 'components/Table/Table.js';
+import Danger from 'components/Typography/Danger.js';
+import React from 'react';
 // react plugin for creating charts
-import ChartistGraph from "react-chartist";
-import { FaCross } from "react-icons/fa";
-import { RiVirusFill } from "react-icons/ri";
-import { useDispatch, useSelector } from "react-redux";
-import { dailySalesChart, emailsSubscriptionChart } from "variables/charts.js";
+import ChartistGraph from 'react-chartist';
+import { FaCross } from 'react-icons/fa';
+import { RiVirusFill } from 'react-icons/ri';
+import { useDispatch, useSelector } from 'react-redux';
+import { dailySalesChart, emailsSubscriptionChart } from 'variables/charts.js';
 
 const useStyles = makeStyles(styles);
 
@@ -43,12 +43,12 @@ function increaseText(increaseRate) {
 export default function Dashboard() {
   const classes = useStyles();
 
-  const data = useSelector((state) => state.dashboard);
+  const data = useSelector(state => state.dashboard);
   console.log(data);
 
   function buildChart(chartData) {
     return {
-      labels: ["", "", "", "", "", "", ""],
+      labels: ['', '', '', '', '', '', ''],
       series: [chartData],
     };
   }
@@ -56,18 +56,18 @@ export default function Dashboard() {
   function buildBarChart(chartData) {
     return {
       labels: [
-        "Jan",
-        "Fev",
-        "Mar",
-        "Abr",
-        "Mai",
-        "Jun",
-        "Jul",
-        "Ago",
-        "Set",
-        "Out",
-        "Nov",
-        "Dez",
+        'Jan',
+        'Fev',
+        'Mar',
+        'Abr',
+        'Mai',
+        'Jun',
+        'Jul',
+        'Ago',
+        'Set',
+        'Out',
+        'Nov',
+        'Dez',
       ],
       series: chartData,
     };
@@ -92,8 +92,8 @@ export default function Dashboard() {
       },
       axisY: {
         labelInterpolationFnc: function (value) {
-          if (value >= 1000) return value / 1000 + "k";
-          if (value >= 1000000) return value / 1000000 + "kk";
+          if (value >= 1000) return value / 1000 + 'k';
+          if (value >= 1000000) return value / 1000000 + 'kk';
           return value;
         },
       },
@@ -101,15 +101,20 @@ export default function Dashboard() {
   }
 
   function buildBarChartOptions(chartData) {
-    const high =
+    let high = chartData[0].reduce(function (a, b) {
+      return Math.max(a, b);
+    });
+
+    if (chartData[1]) {
       Math.max(
-        chartData[0].reduce(function (a, b) {
-          return Math.max(a, b);
-        }),
+        high,
         chartData[1].reduce(function (a, b) {
           return Math.max(a, b);
         })
-      ) + 100;
+      );
+    }
+
+    high += 100;
 
     return {
       lineSmooth: Chartist.Interpolation.cardinal({
@@ -125,8 +130,8 @@ export default function Dashboard() {
       },
       axisY: {
         labelInterpolationFnc: function (value) {
-          if (value >= 1000) return value / 1000 + "k";
-          if (value >= 1000000) return value / 1000000 + "kk";
+          if (value >= 1000) return value / 1000 + 'k';
+          if (value >= 1000000) return value / 1000000 + 'kk';
           return value;
         },
       },
@@ -134,32 +139,32 @@ export default function Dashboard() {
   }
   function monthNumberToString(monthNumber) {
     switch (monthNumber) {
-      case "01":
-        return "Janeiro";
-      case "02":
-        return "Fevereiro";
-      case "03":
-        return "Março";
-      case "04":
-        return "Abril";
-      case "05":
-        return "Maio";
-      case "06":
-        return "Junho";
-      case "07":
-        return "Julho";
-      case "08":
-        return "Agosto";
-      case "09":
-        return "Setembro";
-      case "10":
-        return "Outubro";
-      case "11":
-        return "Novembro";
-      case "12":
-        return "Dezembro";
+      case '01':
+        return 'Janeiro';
+      case '02':
+        return 'Fevereiro';
+      case '03':
+        return 'Março';
+      case '04':
+        return 'Abril';
+      case '05':
+        return 'Maio';
+      case '06':
+        return 'Junho';
+      case '07':
+        return 'Julho';
+      case '08':
+        return 'Agosto';
+      case '09':
+        return 'Setembro';
+      case '10':
+        return 'Outubro';
+      case '11':
+        return 'Novembro';
+      case '12':
+        return 'Dezembro';
       default:
-        return "";
+        return '';
     }
   }
   return (
@@ -167,13 +172,13 @@ export default function Dashboard() {
       <GridContainer>
         <GridItem xs={12} sm={6} md={3}>
           <Card>
-            <CardHeader color="danger" stats icon>
-              <CardIcon color="danger">
+            <CardHeader color='danger' stats icon>
+              <CardIcon color='danger'>
                 <FaCross />
               </CardIcon>
               <p className={classes.cardCategory}>Mortes Confirmadas</p>
               <h3 className={classes.cardTitle}>
-                {data.data.reports ? data.data.reports.BR.deaths : ""}
+                {data.data.reports ? data.data.reports.BR.deaths : ''}
               </h3>
             </CardHeader>
             <CardFooter stats>
@@ -181,7 +186,7 @@ export default function Dashboard() {
                 <Danger>
                   <Warning />
                 </Danger>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                <a href='#pablo' onClick={e => e.preventDefault()}>
                   {increaseText(data.rate_deaths)}
                 </a>
               </div>
@@ -190,13 +195,13 @@ export default function Dashboard() {
         </GridItem>
         <GridItem xs={12} sm={6} md={3}>
           <Card>
-            <CardHeader color="warning" stats icon>
-              <CardIcon color="warning">
+            <CardHeader color='warning' stats icon>
+              <CardIcon color='warning'>
                 <RiVirusFill />
               </CardIcon>
               <p className={classes.cardCategory}>Casos Confirmados</p>
               <h3 className={classes.cardTitle}>
-                {data.data.reports ? data.data.reports.BR.cases : ""}
+                {data.data.reports ? data.data.reports.BR.cases : ''}
               </h3>
             </CardHeader>
             <CardFooter stats>
@@ -204,7 +209,7 @@ export default function Dashboard() {
                 <Danger>
                   <Warning />
                 </Danger>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                <a href='#pablo' onClick={e => e.preventDefault()}>
                   {increaseText(data.rate_cases)}
                 </a>
               </div>
@@ -213,18 +218,18 @@ export default function Dashboard() {
         </GridItem>
         <GridItem xs={12} sm={6} md={3}>
           <Card>
-            <CardHeader color="primary" stats icon>
-              <CardIcon color="primary">
+            <CardHeader color='primary' stats icon>
+              <CardIcon color='primary'>
                 <Icon>info_outline</Icon>
               </CardIcon>
               <p className={classes.cardCategory}>Informações Gerais</p>
-              <Typography variant="body2" className={classes.cardTitle}>
+              <Typography variant='body2' className={classes.cardTitle}>
                 Dias desde o começo: {data.daysSinceStarted}
               </Typography>
-              <Typography variant="body2" className={classes.cardTitle}>
+              <Typography variant='body2' className={classes.cardTitle}>
                 Casos hoje: {data.today_cases}
               </Typography>
-              <Typography variant="body2" className={classes.cardTitle}>
+              <Typography variant='body2' className={classes.cardTitle}>
                 Mortes hoje: {data.today_deaths}
               </Typography>
             </CardHeader>
@@ -236,11 +241,11 @@ export default function Dashboard() {
       <GridContainer>
         <GridItem xs={12} sm={12} md={6}>
           <Card chart>
-            <CardHeader color="success">
+            <CardHeader color='success'>
               <ChartistGraph
-                className="ct-chart"
+                className='ct-chart'
                 data={buildChart(data.lastSevenDays_newCases)}
-                type="Line"
+                type='Line'
                 options={buildChartOptions(data.lastSevenDays_newCases)}
                 listener={dailySalesChart.animation}
               />
@@ -259,11 +264,11 @@ export default function Dashboard() {
         </GridItem>
         <GridItem xs={12} sm={12} md={6}>
           <Card chart>
-            <CardHeader color="success">
+            <CardHeader color='success'>
               <ChartistGraph
-                className="ct-chart"
+                className='ct-chart'
                 data={buildChart(data.lastSevenDays_cases)}
-                type="Line"
+                type='Line'
                 options={buildChartOptions(data.lastSevenDays_cases)}
                 listener={dailySalesChart.animation}
               />
@@ -284,11 +289,11 @@ export default function Dashboard() {
       <GridContainer>
         <GridItem xs={12} sm={12} md={6}>
           <Card chart>
-            <CardHeader color="success">
+            <CardHeader color='success'>
               <ChartistGraph
-                className="ct-chart"
+                className='ct-chart'
                 data={buildChart(data.lastSevenDays_newDeaths)}
-                type="Line"
+                type='Line'
                 options={buildChartOptions(data.lastSevenDays_newDeaths)}
                 listener={dailySalesChart.animation}
               />
@@ -307,11 +312,11 @@ export default function Dashboard() {
         </GridItem>
         <GridItem xs={12} sm={12} md={6}>
           <Card chart>
-            <CardHeader color="success">
+            <CardHeader color='success'>
               <ChartistGraph
-                className="ct-chart"
+                className='ct-chart'
                 data={buildChart(data.lastSevenDays_deaths)}
-                type="Line"
+                type='Line'
                 options={buildChartOptions(data.lastSevenDays_deaths)}
                 listener={dailySalesChart.animation}
               />
@@ -329,18 +334,16 @@ export default function Dashboard() {
           </Card>
         </GridItem>
       </GridContainer>
+
       <GridContainer>
         <GridItem xs={12} sm={12} md={12}>
           <Card chart>
-            <CardHeader color="warning">
+            <CardHeader color='success'>
               <ChartistGraph
-                className="ct-chart"
-                data={buildBarChart([data.months_cases, data.months_deaths])}
-                type="Bar"
-                options={buildBarChartOptions([
-                  data.months_cases,
-                  data.months_deaths,
-                ])}
+                className='ct-chart'
+                data={buildBarChart([data.months_cases])}
+                type='Bar'
+                options={buildBarChartOptions([data.months_cases, ,])}
                 responsiveOptions={emailsSubscriptionChart.responsiveOptions}
                 listener={emailsSubscriptionChart.animation}
               />
@@ -348,7 +351,37 @@ export default function Dashboard() {
             <CardBody>
               <h4 className={classes.cardTitle}>Análise Mensal: Novos Casos</h4>
               <p className={classes.cardCategory}>
-                Mês atual: {monthNumberToString(data.currentMonth)}{" "}
+                Mês atual: {monthNumberToString(data.currentMonth)}{' '}
+              </p>
+            </CardBody>
+            <CardFooter chart>
+              <div className={classes.stats}>
+                <AccessTime /> Atualizado: {data.timeLastUpdated.toString()}
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
+
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card chart>
+            <CardHeader color='success'>
+              <ChartistGraph
+                className='ct-chart'
+                data={buildBarChart([data.months_deaths])}
+                type='Bar'
+                options={buildBarChartOptions([data.months_deaths])}
+                responsiveOptions={emailsSubscriptionChart.responsiveOptions}
+                listener={emailsSubscriptionChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <h4 className={classes.cardTitle}>
+                Análise Mensal: Novas mortes
+              </h4>
+              <p className={classes.cardCategory}>
+                Mês atual: {monthNumberToString(data.currentMonth)}{' '}
               </p>
             </CardBody>
             <CardFooter chart>
@@ -363,7 +396,7 @@ export default function Dashboard() {
       <GridContainer>
         <GridItem xs={12} sm={12} md={6}>
           <Card>
-            <CardHeader color="warning">
+            <CardHeader color='warning'>
               <h4 className={classes.cardTitleWhite}>Estado a estado</h4>
               <p className={classes.cardCategoryWhite}>
                 Novos casos chegam todos os dias
@@ -371,13 +404,13 @@ export default function Dashboard() {
             </CardHeader>
             <CardBody>
               <Table
-                tableHeaderColor="warning"
+                tableHeaderColor='warning'
                 tableHead={[
-                  "Estado",
-                  "Casos",
-                  "Mortes",
-                  "Novos Casos",
-                  "Novas Mortes",
+                  'Estado',
+                  'Casos',
+                  'Mortes',
+                  'Novos Casos',
+                  'Novas Mortes',
                 ]}
                 tableData={data.states}
               />
